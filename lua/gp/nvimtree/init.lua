063d2673a4ffba07f9c850d559d2f5efd479455d
AlexRosas <rosasrias82@gmail.com>
2026-01-12 17:00:18 -0500

    Creqte hl groups (semantic + depth-aware)

--- lua/gp/nvimtree/init.lua
+++ lua/gp/nvimtree/init.lua
@@ -1,9 +1,32 @@
 local M = {}
 
----Setup gp-nvim-tree-icons
+---Setup gp-nvim-tree-icons.
+---
+---This function initializes the plugin lifecycle:
+---  1. Applies user configuration
+---  2. Creates highlight groups (semantic + depth-aware)
+---  3. Re-applies highlights on colorscheme change
+---  4. Monkey-patches nvim-tree to inject icons & highlights
+---
 ---@param opts GpNvimTreeIconsConfig|nil
 function M.setup(opts)
+	-- Apply configuration (with defaults)
 	require("gp.nvimtree.config").setup(opts)
+
+	-- Create highlight groups
+	local highlights = require("gp.nvimtree.highlights")
+	highlights.setup()
+
+	-- Recreate highlights after colorscheme changes
+	-- (most colorschemes clear custom highlight groups)
+	vim.api.nvim_create_autocmd("ColorScheme", {
+		group = vim.api.nvim_create_augroup("GpNvimTreeHighlights", { clear = true }),
+		callback = function()
+			highlights.setup()
+		end,
+	})
+
+	-- Apply nvim-tree DirectoryNode patch
 	require("gp.nvimtree.patch").apply()
 end
 
