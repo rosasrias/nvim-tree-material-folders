f6cc4fd44e4ce5a3140a78a3ec0fb4ff0c3c4562
AlexRosas <rosasrias82@gmail.com>
2026-01-14 16:32:48 -0500

    refactor(nvimtree): unify resolver logic and fix subfamily icon/color priority

--- lua/gp/nvimtree/patch.lua
+++ lua/gp/nvimtree/patch.lua
@@ -1,61 +1,34 @@
--- =========================================================
--- This module monkey-patches nvim-tree DirectoryNode
--- to provide VSCode-like folder icons & highlights.
--- =========================================================
-
 local DirectoryNode = require("nvim-tree.node.directory")
 
-local families = require("gp.nvimtree.families")
-local by_path = require("gp.nvimtree.match_path")
-local by_name = require("gp.nvimtree.match_name")
-local by_sub = require("gp.nvimtree.match_subfamily")
-local hl = require("gp.nvimtree.highlights")
+local resolver = require("gp.nvimtree.resolver")
 local icons = require("gp.nvimtree.icons")
+local hl = require("gp.nvimtree.highlights")
 local cache = require("gp.nvimtree.cache")
 
--- Preserve originals
 local original_icon = DirectoryNode.highlighted_icon
 local original_name = DirectoryNode.highlighted_name
 
 local M = {}
 
 function M.apply()
-	-- =====================================================
-	-- Icon patch
-	-- =====================================================
 	---@diagnostic disable-next-line: duplicate-set-field
 	function DirectoryNode:highlighted_icon()
 		local res = original_icon(self)
 
 		local cached = cache.resolve(self, function()
-			--Resolve subfamily FIRST (most specific)
-			local sub = by_sub.resolve(self)
-
-			--Resolve base family
-			local family = sub and families[sub.inherits] or by_path.resolve(self) or by_name.resolve(self.name)
-
-			if not family then
+			local sem = resolver.resolve(self)
+			if not sem then
 				return
 			end
 
-			--Icon resolution (sub > family)
-			local icon_key = sub and sub.icon_key or family.icon_key
-			local icon_set = icons[icon_key]
+			local icon_set = icons[sem.icon_key]
 			if not icon_set then
 				return
 			end
 
-			local icon = (self.open and icon_set.open) or icon_set.default
-
-			--Highlight resolution (sub color > family color)
-			local color_key = sub and sub.color_key or family.icon_key
-			local highlight = hl.resolve(self, {
-				icon_key = color_key,
-			})
-
 			return {
-				icon = icon,
-				hl = highlight,
+				icon = self.open and icon_set.open or icon_set.default,
+				hl = hl.resolve(self, { icon_key = sem.color_key }),
 			}
 		end)
 
@@ -64,26 +37,18 @@ function M.apply()
 		end
 
 		res.str = cached.icon
-		if cached.hl then
-			res.hl = { cached.hl }
-		end
-
+		res.hl = { cached.hl }
 		return res
 	end
 
-	-- =====================================================
-	-- Name patch (reuse same highlight)
-	-- =====================================================
 	function DirectoryNode:highlighted_name()
 		local res = original_name(self)
 
-		---@diagnostic disable-next-line: missing-parameter
 		local cached = cache.resolve(self)
-		if not cached or not cached.hl then
-			return res
+		if cached and cached.hl then
+			res.hl = { cached.hl }
 		end
 
-		res.hl = { cached.hl }
 		return res
 	end
 end
