0ecad9211b6756c43f1ba2234d91da98b328c89b
AlexRosas <rosasrias82@gmail.com>
2026-01-14 17:04:12 -0500

    refactor(icons): align naming and prepare public icon registry
    - Rename GpIconSet to NtmfIconSet
    - Normalize icon keys (utils, templates)
    - Group icons by semantic domain
    - No visual changes

---
+++ lua/nvim_tree_material_folders/icons.lua
@@ -0,0 +1,163 @@
+-- =========================================================
+-- Icon definitions for nvim-tree-material-folders
+--
+-- Each icon set defines:
+--   - default: closed folder icon
+--   - open:    opened folder icon
+-- =========================================================
+
+---@class NtmfIconSet
+---@field default string
+---@field open string
+
+---@type table<string, NtmfIconSet>
+local icons = {
+
+	-- ===== ASSETS =====
+	images = {
+		default = "󰉏",
+		open = "󱃫",
+	},
+
+	icons = {
+		default = "󰀻",
+		open = "󰀼",
+	},
+
+	fonts = {
+		default = "󰛖",
+		open = "󰛗",
+	},
+
+	-- ===== FRONTEND =====
+	components = {
+		default = "󱧶",
+		open = "󱧷",
+	},
+
+	ui = {
+		default = "󰏘",
+		open = "󰸌",
+	},
+
+	layout = {
+		default = "󰕮",
+		open = "",
+	},
+
+	pages = {
+		default = "󱑾",
+		open = "󱑿",
+	},
+
+	templates = {
+		default = "󱧶",
+		open = "󱧷",
+	},
+
+	hooks = {
+		default = "󱧊",
+		open = "󱧋",
+	},
+
+	types = {
+		default = "󰉍",
+		open = "󱃭",
+	},
+
+	utils = {
+		default = "󰲂",
+		open = "󰲃",
+	},
+
+	-- ===== CORE =====
+	core = {
+		default = "󰣞",
+		open = "󰷎",
+	},
+
+	src = {
+		default = "󰉌",
+		open = "󰮜",
+	},
+
+	app = {
+		default = "󱂵",
+		open = "󱂶",
+	},
+
+	-- ===== BACKEND =====
+	backend = {
+		default = "󱧼",
+		open = "󱧽",
+	},
+
+	database = {
+		default = "󱋣",
+		open = "󱋤",
+	},
+
+	supabase = {
+		default = "󰚝",
+		open = "󰮟",
+	},
+
+	-- ===== STATE =====
+	state = {
+		default = "",
+		open = "󰮝",
+	},
+
+	-- ===== PLATFORM =====
+	platform = {
+		default = "󰌢",
+		open = "󰌣",
+	},
+
+	-- ===== GAME / DATA =====
+	game = {
+		default = "󰐣",
+		open = "󰐤",
+	},
+
+	data = {
+		default = "󰆼",
+		open = "󰆽",
+	},
+
+	-- ===== STYLES =====
+	styles = {
+		default = "󰉘",
+		open = "󰮞",
+	},
+
+	-- ===== TEST / TOOLING =====
+	tests = {
+		default = "󱧸",
+		open = "󱧹",
+	},
+
+	tools = {
+		default = "󰆕",
+		open = "󰆖",
+	},
+
+	-- ===== CONFIG / BUILD =====
+	config = {
+		default = "",
+		open = "󱂀",
+	},
+
+	build = {
+		default = "󰴋",
+		open = "󰴌",
+	},
+
+	-- ===== DOCS =====
+	docs = {
+		default = "󱧺",
+		open = "󱧻",
+	},
+}
+
+return icons
